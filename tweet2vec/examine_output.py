92bc80944d056831c61f0b6d7d83900f1267bcd5
Wilson Cai <wcai@berkeley.edu>
2017-06-08 14:05:28 -0500

    clean

--- tweet2vec/examine_output.py
+++ tweet2vec/examine_output.py
@@ -8,25 +8,11 @@ predictions = np.load('./predictions.npy')
 data = np.load('./data.pkl')
 
 ha.shape
-ha[0].shape
-
-sp.stats.describe(ha[0])
 
 np.sum(ha[0],axis = 1)
-
-np.sum(ha[0][0])
-sp.stats.describe(ha[0][0])
-
-sum(ha[0][0] > .5)
-
 
 targets
 predictions.shape
 data
-
-
-ha[0]
-ha[1]
-
 
 ha > 0.999
